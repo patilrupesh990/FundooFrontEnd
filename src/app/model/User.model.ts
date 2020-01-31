800fdab3d3d8402ea276cce612d34b144f75581a
Rupesh <patilrupesh990@gmail.com>
2020-01-31 19:19:15 +0530

    [Rupesh patil]. added user Model

---
+++ src/app/model/User.model.ts
@@ -0,0 +1,13 @@
+export class UserModel{
+
+    firstname:String;
+    lastname:String;
+    email:String;
+    password:String;
+    userName:String;
+    dateOfBirth:Date;
+    phNo:number;
+    gender:String;
+
+
+}
