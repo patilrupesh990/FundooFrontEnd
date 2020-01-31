c60486aaac8e71c1e040394a9d2f4e9269e99002
Rupesh <patilrupesh990@gmail.com>
2020-01-31 19:24:04 +0530

    [Rupesh patil]. added App module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ReactiveFormsModule, FormBuilder, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { Pipe, PipeTransform } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {UserService} from './services/userservice.service';
 
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule, MatToolbarModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatRadioModule
+} from "@angular/material";
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
