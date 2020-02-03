95bd556e4f94b6041a671a7988f00a8563a3dc0a
Rupesh <patilrupesh990@gmail.com>
2020-02-03 19:02:27 +0530

    [Rupesh-patil].upadeted environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: true,
+  userApiURL:'http://localhost:8086/users',
 
+  registerURL:'register',
+  loginURL:'login',
+  userActivURL:'register/activ',
+  forgotPasswordURL:'verify-user',
+  resetPasswordURL:'reset-password'
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
