5d448dbcaa237cc183874d2a40868943da7496fd
Rupesh <patilrupesh990@gmail.com>
2020-02-11 19:26:43 +0530

    [Rupesh patil].toolbar updated

--- src/app/components/dashboard/toolbar/toolbar.component.ts
+++ src/app/components/dashboard/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { SideNavService } from 'src/app/services/sidenavservice.service';
 
 @Component({
@@ -7,13 +7,22 @@ import { SideNavService } from 'src/app/services/sidenavservice.service';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+  @Output() toggleEvent = new EventEmitter<boolean>();
+  opened:boolean=false;
 
   constructor(private sideNavService: SideNavService) {
   }
+  
+  ontoggel(input:any){
+    console.log("1"+input);
+    this.toggleEvent.emit(input);
+    console.log(input);
+    this.opened=!this.opened;
+  }
+
   ngOnInit() {
   }
-  clickMenu() { 
-    this.sideNavService.toggle();
-  }
+  
+  
 
 }
