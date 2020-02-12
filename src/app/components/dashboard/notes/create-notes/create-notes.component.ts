a4c0ada8dd04bc2ca2b399b9e38b129acabdf5a6
Rupesh <patilrupesh990@gmail.com>
2020-02-12 19:24:46 +0530

    [Rupesh-patil].updated create Note Component

--- src/app/components/dashboard/notes/create-notes/create-notes.component.ts
+++ src/app/components/dashboard/notes/create-notes/create-notes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/model/note.model';
+import {Router,ParamMap} from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NoteserviceService } from 'src/app/services/noteservice.service';
 
 @Component({
   selector: 'app-create-notes',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-notes.component.scss']
 })
 export class CreateNotesComponent implements OnInit {
-
-  constructor() { }
+  notes:Note=new Note();
+  forgotpasswordForm: FormGroup;
+  open:boolean=false
+  constructor(private formBuilder: FormBuilder, private router: Router,private noteService:NoteserviceService) { }
 
   ngOnInit() {
+    this.forgotpasswordForm=this.formBuilder.group({
+      title:['',[Validators.required]],
+      description:['',[Validators.required]]
+    });
   }
-
+  onClickCreateNoteOpen()
+  {
+    this.open=!this.open;
+  }
+  
 }
