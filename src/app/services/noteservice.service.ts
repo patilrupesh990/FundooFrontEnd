bd1d6eaa5a9fc44a990a844561eabc7fef3362f0
Rupesh <patilrupesh990@gmail.com>
2020-02-13 19:15:00 +0530

    [Rupesh patil].updated noteService ts file

--- src/app/services/noteservice.service.ts
+++ src/app/services/noteservice.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaderResponse ,HttpHeaders} from '@angular/common/http';
 import { HttpService } from './httpservice.service';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +13,10 @@ export class NoteserviceService {
   private httpOtions={
     headers: new HttpHeaders ({'content-type':'application/json'})
     };
+    createNote(note:any,token:any):Observable<any>
+    {
+      console.log("token:::::"+sessionStorage.token);
+      console.log(`${environment.notesApiURL}/${environment.createNote}`);
+      return this.httpservice.post(`${environment.notesApiURL}/${environment.createNote}/${localStorage.token}`,note,{headers: new HttpHeaders().set('token', localStorage.token) });
+    }
 }
