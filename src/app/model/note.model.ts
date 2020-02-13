be2b18aa5a735c7d08c9ee96cc768275e2b12732
Rupesh <patilrupesh990@gmail.com>
2020-02-13 19:18:06 +0530

    [Rupesh patil].added note model ts file

--- src/app/model/note.model.ts
+++ src/app/model/note.model.ts
@@ -1,5 +1,13 @@
 export class Note{
     title:String;
     description:String;
+    isTrash: boolean ;
+    isPin: boolean ;
+    isArchive: boolean ;
+    color: String ;
+    reminder: String ;
+    createdTime: String ;
+    updaTimestamp:  String;	
+    userId: number ;
 }
 
