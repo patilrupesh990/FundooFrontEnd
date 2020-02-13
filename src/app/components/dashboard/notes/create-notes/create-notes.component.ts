bea2095f1ca8d66e6c57f9566fa59681e24b8499
Rupesh <patilrupesh990@gmail.com>
2020-02-13 19:17:32 +0530

    [Rupesh patil].updated create-notes component ts file

--- src/app/components/dashboard/notes/create-notes/create-notes.component.ts
+++ src/app/components/dashboard/notes/create-notes/create-notes.component.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/model/note.model';
-import {Router,ParamMap} from '@angular/router';
+import { Router, ParamMap } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NoteserviceService } from 'src/app/services/noteservice.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-create-notes',
   templateUrl: './create-notes.component.html',
-  styleUrls: ['./create-notes.component.scss']
+  styleUrls: ['./create-notes.component.scss'],
+
 })
 export class CreateNotesComponent implements OnInit {
-  notes:Note=new Note();
-  forgotpasswordForm: FormGroup;
-  open:boolean=false
-  constructor(private formBuilder: FormBuilder, private router: Router,private noteService:NoteserviceService) { }
+  note: Note = new Note();
+  open: boolean = false;
+  token: string = sessionStorage.getItem('token');
+
+  constructor(private formBuilder: FormBuilder, private matSnackBar: MatSnackBar, private router: Router, private noteService: NoteserviceService) { }
 
   ngOnInit() {
-    this.forgotpasswordForm=this.formBuilder.group({
-      title:['',[Validators.required]],
-      description:['',[Validators.required]]
-    });
+
   }
-  onClickCreateNoteOpen()
-  {
-    this.open=!this.open;
+  onClickCreateNoteOpen() {
+    this.open = !this.open;
   }
-  
+
+
+  onSubmit() {
+    if (this.note.title) {
+      this.noteService.createNote(this.note, this.token).subscribe(response => {
+        this.note = new Note();
+        console.log(this.note);
+        this.matSnackBar.open('note created', 'ok', { duration: 5000 });
+      },
+        (error: any) => {
+
+          console.log(error)
+          this.matSnackBar.open('error in note creation', 'ok', { duration: 5000 });
+        }
+
+      );
+    }
+  }
 }
