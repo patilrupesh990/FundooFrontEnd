befcdd341e627d0d0bc4386d343fd0e8be10b5c8
Rupesh <patilrupesh990@gmail.com>
2020-02-15 19:03:36 +0530

    [Rupesh patil].updated display-iconList  .ts  file

---
+++ src/app/components/dashboard/notes/notes-iconlist/notes-iconlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-iconlist',
+  templateUrl: './notes-iconlist.component.html',
+  styleUrls: ['./notes-iconlist.component.scss']
+})
+export class NotesIconlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
