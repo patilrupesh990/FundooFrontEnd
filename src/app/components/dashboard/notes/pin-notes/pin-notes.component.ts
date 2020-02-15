3a8788c3f4cbe9717bc2092df9c73fc066c47425
Rupesh <patilrupesh990@gmail.com>
2020-02-15 19:04:17 +0530

    [Rupesh patil].updated pin-notes component

---
+++ src/app/components/dashboard/notes/pin-notes/pin-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pin-notes',
+  templateUrl: './pin-notes.component.html',
+  styleUrls: ['./pin-notes.component.scss']
+})
+export class PinNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
