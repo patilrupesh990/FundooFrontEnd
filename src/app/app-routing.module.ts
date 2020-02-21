0d153b02fa14fd813a548f33e8e2e7c724c53ddf
Rupesh <patilrupesh990@gmail.com>
2020-02-21 19:27:10 +0530

    [Rupesh-patil].updated routing module  file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { from } from 'rxjs';
@@ -12,33 +12,54 @@ import { RegistrationComponent } from './components/authentication/registration/
 import { ForgotpasswordComponent } from './components/authentication/forgotpassword/forgotpassword.component';
 import { ResetpasswordComponent } from './components/authentication/resetpassword/resetpassword.component';
 import { UserActivateComponent } from './components/authentication/user-activate/user-activate.component';
+import { NotesIconlistComponent } from './components/dashboard/notes/notes-iconlist/notes-iconlist.component';
+import { DisplayNotesComponent } from './components/dashboard/notes/display-notes/display-notes.component';
+import { NotesComponent } from './components/dashboard/notes/notes.component';
 
 const routes: Routes = [
-  {path:'register',component:RegistrationComponent},
-  {path:'forgotpassword',component:ForgotpasswordComponent},
-  {path:'login',component:LoginComponent},
-  {path:'',component:LoginComponent},
-  {path:'active/:token',component:UserActivateComponent},
-  {path:'reset-password/:token',component:ResetpasswordComponent},
+  { path: 'register', component: RegistrationComponent },
+  { path: 'forgotpassword', component: ForgotpasswordComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
+  { path: 'active/:token', component: UserActivateComponent },
+  { path: 'reset-password/:token', component: ResetpasswordComponent },
 
   {
-    path:'dashboard/:token',component:DashboardComponent,
-    children:[{
-        path:'toolbar',
-        component:ToolbarComponent
+    path: 'dashboard/:token', component: DashboardComponent,
+    children: [{
+      path: 'toolbar',
+      component: ToolbarComponent
 
     },
     {
-      path:'sidenave',
-      component:SidenavbarComponent
+      path: 'sidenave',
+      component: SidenavbarComponent
     },
     {
-      path:'',
-      component:CreateNotesComponent
-    }
-  ] 
-},
-  {path:'**',component:PageNotFoundComponent},
+      path: '',
+      component: NotesComponent,
+      children: [
+       {
+         path:'',
+         component:CreateNotesComponent
+       } ,
+       {
+        path:'',
+        component:DisplayNotesComponent
+       }
+        ,{
+        path: 'iconlist',
+        component: NotesIconlistComponent
+      }
+    ]
+    },
+
+    {
+      path: ':token/display', component: DisplayNotesComponent
+    },
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent },
 
 ];
 
@@ -47,4 +68,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents=[RegistrationComponent,LoginComponent,UserActivateComponent,ForgotpasswordComponent,DashboardComponent,ResetpasswordComponent,ToolbarComponent]
+export const routingComponents = [RegistrationComponent, LoginComponent, UserActivateComponent, ForgotpasswordComponent, DashboardComponent, ResetpasswordComponent, ToolbarComponent]
