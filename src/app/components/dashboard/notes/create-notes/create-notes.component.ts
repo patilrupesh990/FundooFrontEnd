41cf4d136c2aab322459e1a43902c14d78d3406d
Rupesh <patilrupesh990@gmail.com>
2020-02-21 19:10:38 +0530

    [Rupesh-patil].Added create-notes .ts file

--- src/app/components/dashboard/notes/create-notes/create-notes.component.ts
+++ src/app/components/dashboard/notes/create-notes/create-notes.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Note } from 'src/app/model/note.model';
 import { Router, ParamMap } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NoteserviceService } from 'src/app/services/noteservice.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Note } from 'src/app/model/note.model';
+import { NoteserviceService } from 'src/app/services/noteservice.service';
 
 @Component({
   selector: 'app-create-notes',
@@ -12,14 +12,14 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 })
 export class CreateNotesComponent implements OnInit {
-  note: Note = new Note();
+  // note: Note = new Note();
+  note:Note=new Note();
   open: boolean = false;
   token: string = sessionStorage.getItem('token');
 
   constructor(private formBuilder: FormBuilder, private matSnackBar: MatSnackBar, private router: Router, private noteService: NoteserviceService) { }
 
   ngOnInit() {
-
   }
   onClickCreateNoteOpen() {
     this.open = !this.open;
@@ -27,18 +27,19 @@ export class CreateNotesComponent implements OnInit {
 
 
   onSubmit() {
+    console.log("on submit called"+this.note.title);
     if (this.note.title) {
+      console.log("inside if called");
+
       this.noteService.createNote(this.note, this.token).subscribe(response => {
         this.note = new Note();
         console.log(this.note);
         this.matSnackBar.open('note created', 'ok', { duration: 5000 });
       },
         (error: any) => {
-
           console.log(error)
           this.matSnackBar.open('error in note creation', 'ok', { duration: 5000 });
         }
-
       );
     }
   }
