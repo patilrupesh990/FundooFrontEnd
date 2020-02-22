e148c4e2eaff8aea629508d99e6a05a1ea942bfe
Rupesh <patilrupesh990@gmail.com>
2020-02-22 18:56:03 +0530

    [Rupesh-patil].updated updatednotes component .ts file

--- src/app/components/dashboard/notes/updatenotes/updatenotes.component.ts
+++ src/app/components/dashboard/notes/updatenotes/updatenotes.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
+import { Note } from 'src/app/model/note.model';
+import { NoteserviceService } from 'src/app/services/noteservice.service';
 
 @Component({
   selector: 'app-updatenotes',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./updatenotes.component.scss']
 })
 export class UpdatenotesComponent implements OnInit {
-
-  constructor() { }
-
+note:Note;
+  constructor(public dialogRef: MatDialogRef<UpdatenotesComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,private noteService:NoteserviceService,private snackbar:MatSnackBar) {
+      this.note=this.data.note;
+     }
+    
   ngOnInit() {
   }
+  onSubmit(){
+    this.dialogRef.close();
+    this.noteService.updateNotes(this.note).subscribe((response)=>{
+        this.snackbar.open("Note Updated SuccessFully","ok",{duration:5000});
+    },
+    (error:any)=>{
 
+    }
+    
+    );
+  }
 }
