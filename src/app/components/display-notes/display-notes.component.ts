727d041bb0d4908279783ef91512f07f80582721
Rupesh <patilrupesh990@gmail.com>
2020-02-27 15:35:58 +0530

    [Rupesh-patil].Updated display-notes component

---
+++ src/app/components/display-notes/display-notes.component.ts
@@ -0,0 +1,144 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Note } from 'src/app/model/note.model';
+import { Router, ParamMap, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NoteserviceService } from 'src/app/services/noteservice.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { UpdatenotesComponent } from '../updatenotes/updatenotes.component';
+import { callbackify } from 'util';
+
+@Component({
+  selector: 'app-display-notes',
+  templateUrl: './display-notes.component.html',
+  styleUrls: ['./display-notes.component.scss']
+})
+
+export class DisplayNotesComponent implements OnInit {
+  trashedNotes: boolean = false;
+  archiveNotes: boolean = false;
+  notes: any;
+  pinNotes: any;
+  getAllNotes: [];
+  private sub: any;
+  private param: any;
+
+  constructor(private dialog: MatDialog, private formBuilder: FormBuilder, private matSnackBar: MatSnackBar, private router: Router, private route: ActivatedRoute
+    , private noteService: NoteserviceService, private httpClient: HttpClient) {
+  }
+  ngOnInit() {
+    this.sub = this.route
+      .queryParams
+      .subscribe(params => {
+        this.param = params['page'] || '';
+        if (this.param == "archive") {
+          console.log("elseif archive");
+          this.getAllArchiveNotes();
+        }
+        else if (this.param == "trash") {
+          console.log("elseif trash");
+          this.getAllTrashedNotes();
+        }
+        else {
+          console.log("else display");
+          this.displayNotes();
+        }
+      });
+
+  }
+
+  getAllTrashedNotes() {
+    this.trashedNotes = true;
+    this.archiveNotes = false;
+    console.log("trashed Notes subscribe..");
+    this.noteService.getTrashedNotesList().subscribe(message => {
+      console.log("trashed Notes subscribe..", message.notes);
+      this.notes = message.notes;
+      console.log("final trsah data" + this.trashedNotes);
+    });
+  }
+
+  displayNotes() {
+    this.trashedNotes = false;
+    this.archiveNotes = false;
+    console.log("Display Notes Call");
+    this.noteService.getNotesList().subscribe(message => {
+      this.notes = message.notes;
+      console.log(this.notes);
+    });
+    this.noteService.getPinNotesList().subscribe(message => {
+      console.log("Display PinNotes Call");
+
+      this.pinNotes = message.notes;
+      console.log(this.pinNotes);
+
+    });
+  }
+  getAllArchiveNotes() {
+    this.archiveNotes = true;
+    this.trashedNotes = false;
+    this.noteService.getAllArchiveNote().subscribe((data) => {
+      console.log(data.response);
+      console.log(data);
+      this.notes = data.list;
+    },
+      (error: any) => {
+        this.matSnackBar.open("Error in Dipslay Trashed Notes");
+      });
+  }
+
+
+  onClicksetNoteId(id, isPin) {
+    this.noteService.updateNoteId(id);
+    this.noteService.updateNotePin(isPin);
+  }
+
+  onPin(noteId) {
+    console.log("on pin called");
+
+    this.noteService.pinNotes(noteId).subscribe(response => {
+
+      this.matSnackBar.open('note Pinned', 'ok', { duration: 5000 });
+    },
+      (error: any) => {
+        console.log(error)
+        this.matSnackBar.open('error in note pinned', 'ok', { duration: 5000 });
+      }
+    );
+  }
+
+  openDialog(note): void {
+    console.log("note Id:" + note.id);
+    const dialogRef = this.dialog.open(UpdatenotesComponent, {
+      width: 'auto',
+      panelClass: 'custom-dialog-container',
+      data: { note }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+
+  onClickDelete(noteId: number) {
+    this.noteService.deleteNote(noteId).subscribe(data => {
+      this.matSnackBar.open("Note Deleted", "Ok", { duration: 3000 });
+    },
+      (error) => {
+        this.matSnackBar.open("Error in Note Deletion", "Ok", { duration: 4000 });
+      }
+    );
+  }
+
+  onClickRestore(noteId: number) {
+    this.noteService.restoreNote(noteId).subscribe(data => {
+      this.matSnackBar.open("Note Restored", "Ok", { duration: 3000 });
+    },
+      (error) => {
+        this.matSnackBar.open("Error while Note Restoring", "Ok", { duration: 3000 });
+      }
+    );
+  }
+
+}
