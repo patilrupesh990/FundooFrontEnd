83c84fbf70504f92b2e25aba3892d3fef44032b6
Rupesh <patilrupesh990@gmail.com>
2020-02-28 18:52:33 +0530

    [RupeshPatil].iconlist component .ts file updated

--- src/app/components/notes-iconlist/notes-iconlist.component.ts
+++ src/app/components/notes-iconlist/notes-iconlist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatTooltip, MatSnackBar } from '@angular/material';
 import { NoteserviceService } from 'src/app/services/noteservice.service';
 import { Note } from 'src/app/model/note.model';
+import { LabelService } from 'src/app/services/labelservice.service';
 
 @Component({
   selector: 'app-notes-iconlist',
@@ -12,7 +13,7 @@ export class NotesIconlistComponent implements OnInit {
   @Input() note :Note;
   noteId:number;
   isArchive:boolean=false;
-  constructor(private noteService: NoteserviceService, private snackBar: MatSnackBar) {
+  constructor(private noteService: NoteserviceService,private labelService:LabelService, private snackBar: MatSnackBar) {
    }
   ngOnInit() {
    
@@ -40,9 +41,36 @@ export class NotesIconlistComponent implements OnInit {
       });
   }
 
-  onClickColorPlate(){
+  setColor( color) {
+    console.log("Color---->", color,this.note.id);
+    
+    this.noteService.addColor(this.note.id,color).subscribe(res => {
 
+      console.log("Resssponse backk---->");
 
+      console.log("Response after setting note color-------->", res);
+    })
+  }
+  onclicksetNoteid(noteId:any){
+    console.log(noteId);
+      this.labelService.setlabelList(noteId);
   }
 
+  arrayOfColors = [
+    [
+      { color: "rgb(255, 179, 255)", name: "pink" },
+      { color: "rgb(255, 255, 128)", name: "darkGolden" },
+      { color: "white", name: "white" }
+    ],
+    [
+      { color: "slategray", name: "grey" },
+      { color: "rgb(153, 221, 255)", name: "light blue" },
+      { color: "rgb(200, 232, 104)", name: "yellow" }
+    ],
+    [
+      { color: "rgb(255, 153, 0)", name: "orange" },
+      { color: "rgb(97, 191, 82)", name: "green" },
+      { color: " rgb(158, 136, 191)", name: "darkYellow" }
+    ]
+  ]
 }
