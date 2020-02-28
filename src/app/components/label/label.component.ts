e674aa2353f5118dde941fd114e219d2fe4f6ce8
Rupesh <patilrupesh990@gmail.com>
2020-02-28 18:51:33 +0530

    [RupeshPatil].label component .ts file updated

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LabelService } from 'src/app/services/labelservice.service';
+import { Note } from 'src/app/model/note.model';
+import { Label } from 'src/app/model/label.model';
 
 @Component({
   selector: 'app-label',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./label.component.scss']
 })
 export class LabelComponent implements OnInit {
-
-  constructor() { }
-
+  labels:Label[];
+  noteId:number;
+  constructor(private labelService:LabelService) { }
+  
   ngOnInit() {
+    // this.getAllArchiveNotes();
+    // this.getNoteId();
+    this.getAllUserLabel();
+  }
+  
+  getNoteId(){
+    this.labelService.getlabelList().subscribe(
+      message => {
+        this.noteId = message.labels;
+        console.log("ytytuiyuiyuiyuiyui",this.noteId);  
+    });
+  }
+  getAllUserLabel(){
+    this.labelService.getAllLabels().subscribe((data)=>{
+      this.labels=data.list;
+      console.log(this.labels);
+    });
+  }
+  getAllNoteLabels() {
+    this.labelService.getNoteLabels(this.noteId).subscribe((data)=>{
+    });
+    
   }
 
 }
