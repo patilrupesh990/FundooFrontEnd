899d58afa7d88c73bbc3d0a05c47edf27d4468fb
Rupesh <patilrupesh990@gmail.com>
2020-03-03 19:24:30 +0530

    [Rupesh-patil] modified. sidenavbar component

--- src/app/components/sidenavbar/sidenavbar.component.ts
+++ src/app/components/sidenavbar/sidenavbar.component.ts
@@ -3,6 +3,9 @@ import { MatSidenav } from '@angular/material';
 import { SideNavService } from 'src/app/services/sidenavservice.service';
 import { NoteserviceService } from 'src/app/services/noteservice.service';
 import { Note } from 'src/app/model/note.model';
+import { LabelService } from 'src/app/services/labelservice.service';
+import { Label } from 'src/app/model/label.model';
+import { NoteData } from 'src/app/model/noteData.model';
 @Component({
   selector: 'app-sidenavbar',
   templateUrl: './sidenavbar.component.html',
@@ -11,10 +14,13 @@ import { Note } from 'src/app/model/note.model';
 export class SidenavbarComponent implements OnInit {
   @ViewChild('sidenav',{ static: true }) public sidenav: MatSidenav;
   tab : any = 'tab1';
-  constructor(private sideNavService:SideNavService) { }
+  labels:Label[];
+  notes:NoteData[];
+  constructor(private sideNavService:SideNavService,private labelservice:LabelService) { }
 
   ngOnInit() {
-   
+   this.getLabelList();
+   this.getNotesList();
   }
   changeColor(check){
     // event.srcElement.classList.add("active-color");
@@ -28,6 +34,22 @@ export class SidenavbarComponent implements OnInit {
       this.tab = 'tab3';
     }    
   }
- 
+  
+ getLabelList(){
+    this.labelservice.getLabelList().subscribe(message=>{
+        this.labels=message.label;
+        console.log("side nave labels:",this.labels);
+    });
+ }
+
+ getNotesList(){
+   this.labelservice.getNoteList().subscribe(message=>{
+        this.notes=message.notes;
+        console.log("side nave notes:",this.notes);
+   });
+ }
+ onCLickSetLabelId(labelId){
+    this.labelservice.setLabelId(labelId);
+ }
 
 }
