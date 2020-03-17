1a4d575ceee00ca810e19f87e1fc995613f1e6af
Rupesh <patilrupesh990@gmail.com>
2020-03-17 12:12:05 +0530

    [Rupesh-patil].Updated iconlist

--- src/app/components/notes-iconlist/notes-iconlist.component.ts
+++ src/app/components/notes-iconlist/notes-iconlist.component.ts
@@ -5,6 +5,7 @@ import { Note } from 'src/app/model/note.model';
 import { LabelService } from 'src/app/services/labelservice.service';
 import { AmazingTimePickerService } from 'amazing-time-picker';
 import { CollaboratorComponent } from '../collaborator/collaborator.component';
+import { LabelComponent } from '../label/label.component';
 
 @Component({
   selector: 'app-notes-iconlist',
@@ -12,19 +13,20 @@ import { CollaboratorComponent } from '../collaborator/collaborator.component';
   styleUrls: ['./notes-iconlist.component.scss']
 })
 export class NotesIconlistComponent implements OnInit {
-  @Input() note :Note;
-  noteId:number;
-  isArchive:boolean=false;
+  @Input() note: Note;
+  noteId: number;
+  isArchive: boolean = false;
   public selectedTime = '18:33';
   today: string;
   isoo: any;
-  constructor(private dialog: MatDialog,private noteService: NoteserviceService, private atp: AmazingTimePickerService,private labelService:LabelService, private snackBar: MatSnackBar) {
-   }
+  constructor(private dialog: MatDialog, private noteService: NoteserviceService, private atp: AmazingTimePickerService, private labelService: LabelService, private snackBar: MatSnackBar) {
+   
+  }
   ngOnInit() {
   }
 
   onClickDelete() {
-    this.noteId=this.note.id;
+    this.noteId = this.note.id;
     this.noteService.trashNote(this.note.id).subscribe((response) => {
       this.snackBar.open("Note unpinned and trashed", 'ok', { duration: 5000 });
     },
@@ -35,18 +37,17 @@ export class NotesIconlistComponent implements OnInit {
     );
   }
 
-  onClickArchive(archive:boolean,pin) {
+  onClickArchive(archive: boolean, pin) {
     this.noteService.moveToArchiveNote(this.note.id).subscribe((response) => {
-      console.log("value::",this.note.isArchive,"pin",this.note.isPin);
-      if(this.note.isArchive==true)
-      {
+      console.log("value::", this.note.isArchive, "pin", this.note.isPin);
+      if (this.note.isArchive == true) {
 
         this.snackBar.open("UnArchived", "OK", { duration: 5000 });
       }
-      if(this.note.isPin=true){
+      if (this.note.isPin = true) {
         this.snackBar.open("Note unpinned and Archived", "OK", { duration: 5000 });
       }
-      else{
+      else {
         this.snackBar.open("Note Archived", "OK", { duration: 5000 });
       }
 
@@ -57,7 +58,7 @@ export class NotesIconlistComponent implements OnInit {
   }
 
   open(noteID) {
-    console.log("NoteID to set alram-----",noteID);
+    console.log("NoteID to set alram-----", noteID);
     const amazingTimePicker = this.atp.open({
       time: this.selectedTime,
       theme: 'dark',
@@ -84,67 +85,83 @@ export class NotesIconlistComponent implements OnInit {
       let op = new Date(parseInt(timee[0]), parseInt(timee[1]), parseInt(timee[2]), parseInt(timee[3]), parseInt(timee[4]), parseInt(timee[5]))
       let iso = op.toLocaleString();
       // let iso = op.toLocaleTimeString() 
-      console.log("ISOOO Date--",iso);
-   
-      let dateTime={
-        "id":noteID,
-        "reminder":iso,
-      }
-      console.log("Date time to set reminder--->",dateTime);
+      console.log("ISOOO Date--", iso);
 
-      this.noteService.addReminder(dateTime,iso).subscribe(res => {
+      let dateTime = {
+        "id": noteID,
+        "reminder": iso,
+      }
+      console.log("Date time to set reminder--->", dateTime);
+
+      this.noteService.addReminder(dateTime, iso).subscribe(res => {
         console.log("Response after setting for date time ----------->", res);
-        this.snackBar.open("reminder Added","OK",{duration:3000});
-      })  
+        this.snackBar.open("reminder Added", "OK", { duration: 3000 });
+      })
     });
   }
   dialogCol(): void {
     console.log("Note id in colab111111--->", this.note.id);
 
     const dialogRef = this.dialog.open(CollaboratorComponent, {
-        width: '490px',
+      width: '490px',
       height: '290px',
-  
+
       panelClass: 'custom-dialog-container',
-      data: {  noteId: this.note.id  }
+      data: { noteId: this.note.id }
     });
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
     });
 
     // const dialogRef = this.dialog.open(CollaboratorComponent, {
-     //   data: { noteId: this.note.id }
+    //   data: { noteId: this.note.id }
     // });
 
   }
+  openDialog(note): void {
+    console.log("note Id:" + note.id);
+    const dialogRef = this.dialog.open(LabelComponent, {
+      width: '250px',
+      height: 'auto',
+
+      data: { note }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 
 
-  setColor( color) {
-    console.log("Color---->", color,this.note.id);
-    this.noteService.addColor(this.note.id,color).subscribe(res => {
-      this.snackBar.open("color Added","OK",{duration:3000});
+  setColor(color) {
+    console.log("Color---->", color, this.note.id);
+    this.noteService.addColor(this.note.id, color).subscribe(res => {
+      this.snackBar.open("color Changed", "OK", { duration: 3000 });
     })
   }
-  onclicksetNoteid(noteId:any){
+  onclicksetNoteid(noteId: any) {
     console.log(noteId);
-      this.labelService.setNoteIdForLabel(noteId);
+    this.labelService.setNoteIdForLabel(noteId);
   }
 
   arrayOfColors = [
     [
       { color: "rgb(255, 179, 255)", name: "pink" },
       { color: "rgb(255, 255, 128)", name: "darkGolden" },
-      { color: "white", name: "white" }
+      { color: "rgb(200, 232, 104)", name: "yellow" },
+      { color: " rgb(158, 136, 191)", name: "darkYellow" }
     ],
     [
       { color: "slategray", name: "grey" },
-      { color: "rgb(153, 221, 255)", name: "light blue" },
-      { color: "rgb(200, 232, 104)", name: "yellow" }
+      { color: "rgb(153, 221, 255)", name: "Teal" },
+      { color: "rgb(203,240,248)", name: "blue" },
+      { color: "rgb(174,203,250)", name: "Dark blue" },
     ],
     [
       { color: "rgb(255, 153, 0)", name: "orange" },
       { color: "rgb(97, 191, 82)", name: "green" },
-      { color: " rgb(158, 136, 191)", name: "darkYellow" }
+       { color: "white", name: "white" },
+      { color: " rgb(196,174,251)", name: "purpule" }
+
     ]
   ]
 }
