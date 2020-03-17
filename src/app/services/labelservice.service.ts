e09da770162f4b246df52ae2909e00110cd17ed4
Rupesh <patilrupesh990@gmail.com>
2020-03-17 12:13:46 +0530

    [Rupesh-patil].Updated labelService service.ts

--- src/app/services/labelservice.service.ts
+++ src/app/services/labelservice.service.ts
@@ -3,6 +3,7 @@ import { HttpService } from './httpservice.service';
 import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { HttpHeaders } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -13,8 +14,15 @@ export class LabelService {
   private labelId = new Subject<any>();
   private labelList=new Subject<any>();
   private noteList=new Subject<any>();
-  constructor( private httpservice: HttpService) { }
+  private _autoRefresh$ = new Subject();
+  private notesLabel=new Subject<any>();
+  private labelsNotes=new Subject<any>();
 
+  constructor( private httpservice: HttpService) { }
+  
+  get autoRefresh$() {
+    return this._autoRefresh$;
+  }
 
   getNoteLabels(noteId:any){
     return this.httpservice.get(`${environment.labelApiURL}/${environment.getLabels}?noteId=${noteId}`, { headers: new HttpHeaders().set('token', sessionStorage.token)});
@@ -31,20 +39,42 @@ export class LabelService {
   getNotesByLabel(labelId){
     return this.httpservice.get(`${environment.labelApiURL}/${environment.getNotesByLabelId}?labelId=${labelId}`,{headers:new HttpHeaders().set('token',sessionStorage.token)});
   }
-  
-  //Subject Getters And Setters
 
+  getLabelsByNoteId(noteId){
+    return this.httpservice.get(`${environment.labelApiURL}/${environment.getLabelsByNoteId}?noteID=${noteId}`,{headers:new HttpHeaders().set('token',sessionStorage.token)});
+  }
+    //Subject Getters And Setters
   setNoteIdForLabel(message:any){
-
       this.noteId.next({labels:message});
   }
   getNoteIdForLabel(): Observable<any> {
     console.log("trashNote Service Get");
     return this.noteId.asObservable();
   }
+  
+  setlabelsNotes(message:any){
+    return this.labelsNotes.next({notes:message});
+  }
+  getlabelsNotes():Observable<any>{
+    return this.labelsNotes.asObservable();
+  }
+
   setLabelId(message:any){
     this.labelList.next({labels:message});
   }
+  createLabel(label){
+    return this.httpservice.post(`${environment.labelApiURL}/${environment.createLabel}`,label,{headers:new HttpHeaders().set('token',sessionStorage.token)}).pipe(tap(()=>{
+      this.autoRefresh$.next();
+    }));
+  }
+
+  deleteLabel(label){
+    return this.httpservice.delete(`${environment.labelApiURL}/${environment.deleteLabel}?labelId=${label}`,{headers:new HttpHeaders().set('token',sessionStorage.token)}).pipe(tap(()=>{
+      this.autoRefresh$.next();
+    }));
+  }
+
+  
   getLabelId(message:any):Observable<any>{
     return this.labelList.asObservable();
   }
@@ -60,6 +90,13 @@ export class LabelService {
   }
   getNoteList(){
     return this.noteList.asObservable();
+  }
+  
+  setNoteLabelList(message:any){
+    this.notesLabel.next({notes:message});
+  }
+  getNoteLabelList(){
+    return this.notesLabel.asObservable();
   }
   
 
