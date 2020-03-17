977b08282531b4fd0432ebebf7d134b6e3bd54ad
Rupesh <patilrupesh990@gmail.com>
2020-03-17 12:12:32 +0530

    [Rupesh-patil].Updated label component

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, Inject, ViewChild } from '@angular/core';
 import { LabelService } from 'src/app/services/labelservice.service';
 import { Note } from 'src/app/model/note.model';
 import { Label } from 'src/app/model/label.model';
-import { MatSnackBar } from '@angular/material';
+import { MatSnackBar, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { NoteData } from 'src/app/model/noteData.model';
 
 @Component({
@@ -12,19 +12,26 @@ import { NoteData } from 'src/app/model/noteData.model';
 })
 export class LabelComponent implements OnInit {
 
-
-  notes:NoteData[];
+  notes: NoteData[];
   labels: Label[];
   noteId: number;
   labelId;
-  constructor(private labelService: LabelService,private matSnackBar:MatSnackBar) { }
+  lname:string="";
+
+  constructor(public dialogRef: MatDialogRef<LabelComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,private labelService: LabelService, private matSnackBar: MatSnackBar) { 
+      console.log("constructpor",data.note.id);
+      this.noteId=data.note.id;
+      this.getAllUserLabel();
+    }
 
   ngOnInit() {
-    // this.getAllArchiveNotes();
-    // this.getNoteId();
-    this.getAllUserLabel();
+    this.labelService.autoRefresh$.subscribe(()=>{
+      this.getAllUserLabel();
+    })
+    
     this.getNoteId();
-    // this.getNotesList();
+    
   }
 
   getNoteId() {
@@ -35,38 +42,50 @@ export class LabelComponent implements OnInit {
       });
   }
   getAllUserLabel() {
-    this.labelService.getAllLabels().subscribe((data) => {
-      this.labels = data.list;
+    this.labelService.getAllLabels().subscribe((response) => {
+      console.log("label list", response);
+      this.labels = response.list;
       this.setLabelList();
     });
   }
-  getNotesList(labelId){
-    this.labelService.getNotesByLabel(10).subscribe((data)=>{
-      this.notes=data;
+  getNotesList(labelId) {
+    this.labelService.getNotesByLabel(10).subscribe((data) => {
+      this.notes = data;
       this.setNotesList();
-      console.log("notes list by labelid",data);
+      console.log("notes list by labelid", data);
     });
   }
-  onCheckedAddLabel(labelId,noteId) {
+  onCheckedAddLabel(labelId, noteId) {
     console.log("mat on checked label called");
-    this.labelService.addLabel(this.noteId,labelId,).subscribe((data)=>{
-        this.matSnackBar.open(data.response,"Ok",{duration:3000});
+    this.labelService.addLabel(this.noteId, labelId).subscribe((data) => {
+      this.matSnackBar.open(data.response, "Ok", { duration: 3000 });
     },
-    (error)=>{
-      this.matSnackBar.open("error","Ok",{duration:3000});
+      (error) => {
+        this.matSnackBar.open("error", "Ok", { duration: 3000 });
+      });
+  }
+  onClickCreateLabel(InputLabel){
+    // document.getElementById("textfield1").value = "";
+    console.log(InputLabel,"label input");
+    let label={
+      "labelName":InputLabel
     }
-    );
-   }
-
+      this.labelService.createLabel(label).subscribe((response)=>{
+        this.matSnackBar.open("Label Created","Ok",{duration:3000});
+      });
+  }
+  
   setLabelList() {
     this.labelService.setLabelList(this.labels);
   }
-  setNotesList(){
+  setNotesList() {
     this.labelService.setNoteList(this.notes);
   }
-  getLabelId(){
-    this.labelService.getLabelId((message=>{
+  getLabelId() {
+    this.labelService.getLabelId((message => {
       this.getNotesList(message);
     }));
   }
+
+
 }
